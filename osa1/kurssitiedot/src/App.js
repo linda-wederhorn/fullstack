b902bfc5856f6b80df6e840fbeafeb4e856a78ce
linda-wederhorn <linda.wederhorn@elekta.com>
2023-02-22 14:31:35 +0200

    1.4: kurssitiedot step4

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -8,30 +8,36 @@ const Part = (props) => (
 
 const Content = (props) => (props.parts.map(part => <Part name={part.name} exercises={part.exercises} />))
 
-const Total = (props) => (
-  <p>Number of exercises {props.total}</p>
-)
+const Total = (props) => {
+  let sum = 0
+  props.parts.forEach(part => sum = sum + part.exercises)
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </>
   )
 }
