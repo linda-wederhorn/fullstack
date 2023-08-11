2c8ae32c00c5e5625952576c80413bfe9a672ec1
linda-wederhorn <linda.wederhorn@elekta.com>
2023-08-11 15:42:55 +0300

    2.8: puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,31 +1,50 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-5061815" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
-  const addName = (event) => {
-    event.preventDefault();
-    const nameObject = {
-      name: newName,
-    };
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
 
-    setPersons(persons.concat(nameObject));
-    setNewName("");
-    console.log(persons);
+  const nameInPhonebook = (name) =>
+    persons.some((person) => person.name == name);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    if (nameInPhonebook(newName))
+      alert(`${newName} is already added to phonebook`);
+    else {
+      const personObject = {
+        name: newName,
+        number: newNumber,
+      };
+
+      setPersons(persons.concat(personObject));
+      setNewName("");
+      setNewNumber("");
+    }
   };
 
   return (
     <div>
       <div>newName: {newName}</div>
+      <div>newNumber: {newNumber}</div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -34,7 +53,9 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>
+            {person.name} {person.number}
+          </li>
         ))}
       </ul>
     </div>
