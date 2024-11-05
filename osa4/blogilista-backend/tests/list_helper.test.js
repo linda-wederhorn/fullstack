e01c434c50c17724169af0acfc59c9640a66138a
linda-wederhorn <linda.wederhorn@elekta.com>
2024-11-05 14:19:29 +0200

    4.5*: apufunktioita ja yksikkötestejä, step3

--- osa4/blogilista-backend/tests/list_helper.test.js
+++ osa4/blogilista-backend/tests/list_helper.test.js
@@ -1,6 +1,7 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const testBlogList = require('./fixtures/blogs')
 
 test('dummy returns one', () => {
   const result = listHelper.dummy()
@@ -8,8 +9,6 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-  const testBlogList = require('./fixtures/blogs')
-
   test('of empty list is zero', () => {
     assert.strictEqual(listHelper.totalLikes([]), 0)
   })
@@ -22,5 +21,34 @@ describe('total likes', () => {
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(testBlogList)
     assert.strictEqual(result, 36)
+  })
+})
+
+describe('favorite blog', () => {
+  const { author, title, likes } = testBlogList[2]
+  const maxLikesBlog = { author, title, likes }
+
+  test('returns the blog with most likes', () => {
+    const result = listHelper.favoriteBlog(testBlogList)
+    assert.deepStrictEqual(result, maxLikesBlog)
+  })
+
+  test('returns the first occurrence of blogs with maximum likes', () => {
+    const testBlogListExtended = [{
+      _id: '5a422bc61b54a676234d17fe',
+      title: 'Type wars part 2',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 12,
+      __v: 0
+    }, ...testBlogList]
+
+    const firstFavoriteBlog = {
+      title: 'Type wars part 2',
+      author: 'Robert C. Martin',
+      likes: 12
+    }
+    const result = listHelper.favoriteBlog(testBlogListExtended)
+    assert.deepStrictEqual(result, firstFavoriteBlog)
   })
 })
--- osa4/blogilista-backend/utils/list_helper.js
+++ osa4/blogilista-backend/utils/list_helper.js
@@ -2,6 +2,13 @@ function dummy() { return 1 }
 
 function totalLikes(blogs) { return (blogs.reduce((likesSum, blog) => likesSum + blog.likes, 0)) }
 
+function favoriteBlog(blogs) {
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+  const { author, title, likes } = blogs.find(blog => blog.likes === maxLikes)
+  const maxLikesBlog = { author, title, likes }
+  return maxLikesBlog
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
